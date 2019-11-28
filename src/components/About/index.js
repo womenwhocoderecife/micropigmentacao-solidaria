b1cc270fdf51573a576edd0bfa38ea937e924bb0
cecivieira <cecivieira@gmail.com>
2019-11-28 18:50:52 -0300

    Delete old title

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,25 +2,31 @@ import React from "react";
 import "./style.css";
 
 const About = () => <section className="about">
-    About
+
     <div className="about-text">
+
         <div className="about-text-title">
             <h2>
                 Sobre Nós
             </h2>
         </div>
+
         <div className="about-text-paragraph">
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat  incididunt ut laboret.
                 <br />
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat  incididunt ut laboret.
             </p>
-        </div>
+        </div> 
+
     </div>
+
     <div className="about-image">
+
         <div className="about-image-src">
             <img src="..\..\assets\about.png" />
         </div>
+        
     </div>
 </section>;
 
