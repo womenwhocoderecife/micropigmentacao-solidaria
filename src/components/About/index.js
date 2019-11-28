a631c61c2f3cae7b42106e01bc8a63b9d24a3644
cecivieira <cecivieira@gmail.com>
2019-11-28 19:19:09 -0300

    Review class name

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,15 +4,15 @@ import about from "../assets/img/about.png";
 
 const About = () => <section className="about">
 
-    <div className="about-text">
+    <div className="about__text">
 
-        <div className="about-text-title">
+        <div className="about__text__title">
             <h2>
                 Sobre Nós
             </h2>
         </div>
 
-        <div className="about-text-paragraph">
+        <div className="about__text__paragraph">
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat  incididunt ut laboret.
                 <br />
@@ -22,9 +22,9 @@ const About = () => <section className="about">
 
     </div>
 
-    <figure className="about-image">
-        
-            <img className="about-image-src" src={about} alt="Três mulheres sorrindo" />
+    <figure className="about__image">
+
+            <img className="about__image__src" src={about} alt="Três mulheres sorrindo" />
 
     </figure>
 </section>;
