10afad351ea0583cbaea142dbdc69b37da11e7dd
cecivieira <cecivieira@gmail.com>
2019-11-28 17:00:17 -0300

    Add divs about-text and about-image in the section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./style.css";
 
-const About = () => <section className="about">About</section>;
+const About = () => <section className="about">
+    About
+    <div className="about-text">
+    </div>
+    <div className="about-image"></div>
+</section>;
 
 export default About;
