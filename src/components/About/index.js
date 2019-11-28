f3821f7a03cf98bfbb917dbfb81817fb32332dc3
cecivieira <cecivieira@gmail.com>
2019-11-28 17:01:31 -0300

    Add div about-text-title in div about-text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,6 +4,11 @@ import "./style.css";
 const About = () => <section className="about">
     About
     <div className="about-text">
+        <div className="about-text-title">
+            <h2>
+                Sobre Nós
+            </h2>
+        </div>
     </div>
     <div className="about-image"></div>
 </section>;
