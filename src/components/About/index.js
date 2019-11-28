e93906c0c172c0fe02d613a0cabc2a486fdce488
cecivieira <cecivieira@gmail.com>
2019-11-28 17:02:27 -0300

    Add div about-text-paragraph in div about-text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,6 +9,13 @@ const About = () => <section className="about">
                 Sobre Nós
             </h2>
         </div>
+        <div className="about-text-paragraph">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat  incididunt ut laboret.
+                <br />
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat  incididunt ut laboret.
+            </p>
+        </div>
     </div>
     <div className="about-image"></div>
 </section>;
