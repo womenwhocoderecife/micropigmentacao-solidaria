b6d31d23ddf349f553327855184f001f6f698673
Keila Viana <keilavg72@gmail.com>
2019-12-01 23:09:05 -0300

    adding first testimonial

--- src/components/ImpactedWomen/index.js
+++ src/components/ImpactedWomen/index.js
@@ -1,6 +1,23 @@
 import React from "react"; 
 import "./style.css";
 
-const ImpactedWomen = () => <impactedWomen className="impactedWomen">Impacted</impactedWomen>;
+
+const ImpactedWomen = () => (
+ <impactedWomen className="impactedWomen">
+   <div class ="Container1">
+     <h1>Mulheres Impactadas</h1>
+     <div class= "Depoimento1">
+       <div class = "container">
+         <div>
+           <img scr ="../assets/image.png"></img>
+         </div>
+         <div class = "content">
+           <h2>Nome</h2>
+           <p>texto</p>
+         </div>
+       </div>
+     </div>
+   </div>
+  </impactedWomen>);
 
 export default ImpactedWomen;
