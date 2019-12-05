a224ccba6c9cad3d0773cff41542894297777433
Keila Viana <keilavg72@gmail.com>
2019-12-05 13:13:26 -0300

    add classes in components

--- src/components/ImpactedWomen/index.js
+++ src/components/ImpactedWomen/index.js
@@ -5,33 +5,33 @@ import "./style.css";
 
 const ImpactedWomen = () => (
  <impactedWomen className="impactedWomen">
-   <div class ="Container">
+  
      <h1 className = "title">Mulheres Impactadas</h1>
      <div class= "Depoimento1">
-       <div class = "container">
+       <div class = "container1">
          <div>
-           <img src = "/home/keila/Documentos/github/micropigmentacao-solidaria/assets/image.png"/>
+           <img className= "image1" src = "/home/keila/Documentos/github/micropigmentacao-solidaria/assets/image.png"/>
          </div>
          <div class = "content">
-           <h2>Nome</h2>
-           <p>texto</p>
+           <h2 className = "name1">Nome</h2>
+           <p className = "about1">texto</p>
          </div>
        </div>
-     </div>
+     </div>  
 
      <div class= "Depoimento2">
-       <div class = "container">
+       <div class = "container2">
          <div>
-           <img src = {"/home/keila/Documentos/github/micropigmentacao-solidaria/assets/jane.png"}/>
+           <img className= "image2" src = {"/home/keila/Documentos/github/micropigmentacao-solidaria/assets/jane.png"}/>
          </div>
          <div class = "content">
-           <h2>Nome</h2>
-           <p>texto</p>
+           <h2 className= "name2">Nome</h2>
+           <p className="about2">texto</p>
          </div>
        </div>
      </div>
 
-   </div>
+   
   </impactedWomen>);
 
 export default ImpactedWomen;
