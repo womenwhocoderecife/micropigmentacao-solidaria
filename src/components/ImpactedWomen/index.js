0181c9aa6ae4b17bf023ffdb592f5ba433cba6be
Keila Viana <keilavg72@gmail.com>
2019-12-05 12:11:48 -0300

    adding second testimonial

--- src/components/ImpactedWomen/index.js
+++ src/components/ImpactedWomen/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import "./style.css";
 
 
+
 const ImpactedWomen = () => (
  <impactedWomen className="impactedWomen">
-   <div class ="Container1">
+   <div class ="Container">
      <h1>Mulheres Impactadas</h1>
      <div class= "Depoimento1">
        <div class = "container">
          <div>
-           <img scr ="../assets/image.png"></img>
+           <img src = "/home/keila/Documentos/github/micropigmentacao-solidaria/assets/image.png"/>
          </div>
          <div class = "content">
            <h2>Nome</h2>
@@ -17,6 +18,19 @@ const ImpactedWomen = () => (
          </div>
        </div>
      </div>
+
+     <div class= "Depoimento2">
+       <div class = "container">
+         <div>
+           <img src = {"/home/keila/Documentos/github/micropigmentacao-solidaria/assets/jane.png"}/>
+         </div>
+         <div class = "content">
+           <h2>Nome</h2>
+           <p>texto</p>
+         </div>
+       </div>
+     </div>
+
    </div>
   </impactedWomen>);
 
