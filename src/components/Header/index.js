7437b2d3ee7b89583f0508a1f32b1dc133244b15
Carvalho <aline.de.carvalho@accenture.com>
2019-12-06 17:44:01 -0300

    started html header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,57 +1,41 @@
 import React, { Component } from 'react';
 
 import './style.css';
-import logo from '../assets/img/logo.png';
+import logo from '../../assets/img/logo.png';
 
-const Header = () => 
+const Header = () =>
 
-<div className="menu-container">
-    <nav className="menu">
-        <figure className="menu_figure">
-            <img
-            className="menu_img"
-            src={logo}
-            alt= "Logo Valentes e Bonitas"
-            />
-        </figure>
-        <ul className="menu_list">
-            <li className="menu__list__item">
-                <a className="menu__list__link" href="#home">
-                HOME
-                </a>
-            </li>
-            <li className="menu__list__item">
-                <a className="menu__list__link" href="#about">
-                SOBRE
-                </a>
-            </li>
-            <li className="menu__list__item">
-                <a className="menu__list__link" href="#whoDoes">
-                QUEM FAZ
-                </a>
-            </li>
-            <li className="menu__list__item">
-                <a className="menu__list__link" href="#impactedWomen">
-                MULHERES IMPACTADAS
-                </a>
-            </li>
-            <li className="menu__list__item">
-                <a className="menu__list__link" href="#howToParticipate">
-                COMO PARTICIPAR
-                </a>
-            </li>
-            <li className="menu__list__item">
-                <a className="menu__list__link" href="#faq">
-                FAQ
-                </a>
-            </li>
-            <li className="menu__list__item">
-                <a className="menu__list__link" href="#howToHelp">
-                COMO AJUDAR
-                </a>
-            </li>
-        </ul>
-    </nav>
-</div>
-
+    <header class="menu_nav">
+        <div class="logo">
+            <a href="#inicio">
+                <img className="menu_img" src={logo} alt="Logo Valentes e Bonitas" />
+            </a>
+        </div>
+        <nav class="menu">
+            <ul>
+                <li className="menu_list_item">
+                    <a className="menu_list_link" href="#home"> HOME </a>
+                </li>
+                <li className="menu_list_item">
+                    <a className="menu_list_link" href="#about"> SOBRE </a>
+                </li>
+                <li className="menu_list_item">
+                    <a className="menu_list_link" href="#whoDoes"> QUEM FAZ </a>
+                </li>
+                <li className="menu_list_item">
+                    <a className="menu_list_link" href="#impactedWomen"> MULHERES IMPACTADAS </a>
+                </li>
+                <li className="menu_list_item">
+                    <a className="menu_list_link" href="#howToParticipate"> COMO PARTICIPAR </a>
+                </li>
+                <li className="menu_list_item">
+                    <a className="menu_list_link" href="#faq"> FAQ </a>
+                </li>
+                <li className="menu_list_item">
+                    <a className="menu_list_link" href="#howToHelp"> COMO AJUDAR </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    
 export default Header;
