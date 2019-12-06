cd21a9e03c7aceeefdbfbec4c1ca2e9da9a181d1
Deborah Almeida <deborahsoares01@gmail.com>
2019-12-06 17:46:24 -0200

    index e style Hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+<<<<<<< HEAD
 import icone from "./assets/icone_play.png"
 
 const Hero = () => (
@@ -16,5 +17,32 @@ const Hero = () => (
      </section>   
                 
 );
+=======
+import "../assets"
+
+const Hero = () => (
+    
+    <body>
+    <header class="container-hero">
+            <style>
+                    @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+            </style>
+            <style>
+                    @import url('https://fonts.googleapis.com/css?family=Nunito&display=swap');
+            </style>
+            <h1>Bonita é ser <br/>valente</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur  elit, 
+                sed do eiut <br/>labore et dolore magna aliqua. 
+                Ut enim ad minim veniam.</p>
+        <a>
+                <div id='scroll'> Scroll Down</div>
+                <img id='icone' src="assets/icone_play.png"/>
+        </a>
+            
+    </header>
+    
+    </body>
+)
+>>>>>>> index e style Hero
 
 export default Hero;
