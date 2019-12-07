6e9b7077e61aa463b751b20e4ac7279caddd8b2e
Deborah Almeida <deborahsoares01@gmail.com>
2019-12-07 12:50:32 -0200

    Ajustes no Hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,29 +1,20 @@
 import React from "react";
 import "./style.css";
-import "../assets"
+import icone from "./assets/icone_play.png"
 
 const Hero = () => (
     
-    <body>
-    <header class="container-hero">
-            <style>
-                    @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
-            </style>
-            <style>
-                    @import url('https://fonts.googleapis.com/css?family=Nunito&display=swap');
-            </style>
+    <section class="container-hero">
             <h1>Bonita é ser <br/>valente</h1>
             <p>Lorem ipsum dolor sit amet, consectetur  elit, 
                 sed do eiut <br/>labore et dolore magna aliqua. 
                 Ut enim ad minim veniam.</p>
-        <a>
-                <div id='scroll'> Scroll Down</div>
-                <img id='icone' src="assets/icone_play.png"/>
+        <a href="">
+                <div className='scroll'> Scroll Down </div>
+                <img src= {icone} alt="Icone_rolagem_para_baixo" />
         </a>
-            
-    </header>
-    
-    </body>
+     </section>   
+                
 )
 
 export default Hero;
