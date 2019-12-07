d77ba4f702c0c5765079b04888b6a0b039a11806
cecivieira <cecivieira@gmail.com>
2019-12-07 15:05:30 -0300

    Delete all code

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,6 +4,6 @@ import facebook from "../../assets/icons/twitter.svg";
 import instagram from "../../assets/icons/instagram.svg";
 import logo from "../../assets/images/logo.svg";
 
-const Footer = () => <section className="about">Footer</section>
+const Footer = () => <section className="about">Footer</section>;
 
 export default Footer;
