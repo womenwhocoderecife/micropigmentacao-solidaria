4428d1abf9296c93780088002805a42b26edfaa3
Keila Viana <keilavg72@gmail.com>
2019-12-12 17:58:50 -0300

    add first women + title

--- src/components/ImpactedWomen/index.js
+++ src/components/ImpactedWomen/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import "./style.css";
+import woman1 from "../../assets/video1.png";
 
 const ImpactedWomen = () => <section className="impactedwomen">
     
-    
+    <h1 className = "title">Mulheres Impactadas</h1>
+     <div class= "Depoimento1">
+       <div class = "container">
+         <div>
+           <img className= "image1" src = {woman1}/>
+         </div>
+         <div class = "content">
+           <h2 className = "name">Nome</h2>
+           <p className = "History">texto</p>
+         </div>
+       </div>
+     </div>
     
     
     </section>;
