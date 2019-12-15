1ad7c1d031c50fd7f0e5d9759d18fd6216a0a24d
cecivieira <cecivieira@gmail.com>
2019-12-15 19:52:00 -0300

    Add logo and social media list

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,6 +4,33 @@ import facebook from "../../assets/icons/twitter.svg";
 import instagram from "../../assets/icons/instagram.svg";
 import logo from "../../assets/images/logo.svg";
 
-const Footer = () => <section className="about">Footer</section>;
+const Footer = () => <section className="about">
+    <div className="footer__container">
+      
+      <div className="footer__list__logo">
+        <figure className="footer__figure__logo">
+          <img className="footer__logo" scr={logo} alt="logo" title="Valentes e Bonitas" />
+        </figure>
+        <ul className="footer__list__social-network">
+          <li className="footer__list__item__social-network">
+            <a href="">
+              <figure className="footer__figure__social-network">
+                <img className="footer__img__social-network" src={facebook} alt="facebook" title="facebook" />
+              </figure>
+            </a>
+          </li>
+          <li className="footer__list__item__social-network">
+            <a href="">
+              <figure className="footer__figure__social-network">
+                <img className="footer__img__social-network" src={instagram} alt="instagram" title="instagram" />
+              </figure>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      
+    </div>
+</section>;
 
 export default Footer;
