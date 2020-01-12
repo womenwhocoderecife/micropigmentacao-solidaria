5e7e19f4a42d9763b4936b3353346f53128d168b
alinecbsr <alinecbsr@gmail.com>
2020-01-12 15:17:06 -0300

    Alterado texto do menu para letras minúsculas; Ajustado 'menu_list'; Removido duplicidade de 'menu_list_link' e ajustado propriedades.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,25 +15,25 @@ const Header = () =>
         <nav className="menu">
             <ul className="menu_list">
                 <li className="menu_list_item">
-                    <a className="menu_list_link" href="#home"> HOME </a>
+                    <a className="menu_list_link" href="#home">home</a>
                 </li>
                 <li className="menu_list_item">
-                    <a className="menu_list_link" href="#about"> SOBRE </a>
+                    <a className="menu_list_link" href="#about">sobre</a>
                 </li>
                 <li className="menu_list_item">
-                    <a className="menu_list_link" href="#whoDoes"> QUEM FAZ </a>
+                    <a className="menu_list_link" href="#whoDoes">quem faz</a>
                 </li>
                 <li className="menu_list_item">
-                    <a className="menu_list_link" href="#impactedWomen"> MULHERES IMPACTADAS </a>
+                    <a className="menu_list_link" href="#impactedWomen">mulheres impactadas</a>
                 </li>
                 <li className="menu_list_item">
-                    <a className="menu_list_link" href="#howToParticipate"> COMO PARTICIPAR </a>
+                    <a className="menu_list_link" href="#howToParticipate">como participar</a>
                 </li>
                 <li className="menu_list_item">
-                    <a className="menu_list_link" href="#faq"> FAQ </a>
+                    <a className="menu_list_link" href="#faq">faq</a>
                 </li>
                 <li className="menu_list_item">
-                    <a className="menu_list_link" href="#howToHelp"> COMO AJUDAR </a>
+                    <a className="menu_list_link" href="#howToHelp">como ajudar</a>
                 </li>
             </ul>
         </nav>
