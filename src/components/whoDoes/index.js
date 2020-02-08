c96bbe4e51f3cf17f0c26df1c1950e565816dd22
cecivieira <cecivieira@gmail.com>
2020-02-08 13:49:17 -0300

    add images nas classes body and team

--- src/components/whoDoes/index.js
+++ src/components/whoDoes/index.js
@@ -2,30 +2,44 @@ import React from "react";
 import "./style.css";
 import daniela from "../assets/img/daniela.png";
 import yone from "../assets/img/yone.jpg";
-import woman1 from "../assets/img/woman1.jpg";
-import woman2 from "../assets/img/woman2.jpg";
+import woman1 from "../assets/img/woman1.JPG";
+import woman2 from "../assets/img/woman2.JPG";
 import facebook from "../assets/img/Facebook.svg"
 import Instagram from "../assets/img/Instagram.svg"
 import Twitter from "../assets/img/Twitter.svg"
 
 
-const whoDoes = () => <section className="whoDoes">
+const WhoDoes = () => <section className="whoDoes">
     <h1 className="title">Quem Faz</h1>
     <div className="whoDoes__body">
         <figure className="whoDoes__body__figure">
 
+            <img className="whoDoes__image__src" src={yone} alt="yone" />
+
         </figure>
         <div className="whoDoes__body__text">
             <h1 className="whoDoes__body__title">
+            Yone Mendes
             </h1>
             <h2 className="whoDoes__body__subtitle">
+            Esteticista
             </h2>
             <p className="whoDoes__body__paragraph">
+            Lottie is graduated from Hardvard dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </p>
             <div className="whoDoes__body__text__icons">
+            <img className="about__icon__src" src={Twitter} alt="twitter" />
+            <img className="about__icon__src" src={facebook} alt="facebook" />
+            <img className="about__icon__src" src={Instagram} alt="instagram" />
             </div>
         </div>
+
         <div className="whoDoes__team">
+        <img className="about__image-team__src" src={yone} alt="yone" />
+        <img className="about__image-team__src" src={daniela} alt="daniela" />
+        <img className="about__image-team__src" src={} alt="" />
+        <img className="about__image-team__src" src={} alt="" />
         </div>
         
 
@@ -34,4 +48,4 @@ const whoDoes = () => <section className="whoDoes">
 
 </section>;
 
-export default whoDoes;
+export default WhoDoes;
