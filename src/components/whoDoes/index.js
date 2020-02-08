71eae9a1e734ee25ac39a516ee82735e66ce44ce
cecivieira <cecivieira@gmail.com>
2020-02-08 13:10:35 -0300

    import images

--- src/components/whoDoes/index.js
+++ src/components/whoDoes/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./style.css";
+import daniela from "../assets/img/daniela.png";
+import yone from "../assets/img/yone.jpg";
+import woman1 from "../assets/img/woman1.jpg";
+import woman2 from "../assets/img/woman2.jpg";
+
 
 const whoDoes = () => <section className="whoDoes">
     <h1 className="title">Quem Faz</h1>
     <div className="whoDoes__body">
         <figure className="whoDoes__body__figure">
+
         </figure>
         <div className="whoDoes__body__text">
             <h1 className="whoDoes__body__title">
