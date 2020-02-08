e02d3b0691e2089def9914c1a61d5408d6235293
cecivieira <cecivieira@gmail.com>
2020-02-08 13:01:08 -0300

    add structure

--- src/components/whoDoes/index.js
+++ src/components/whoDoes/index.js
@@ -2,7 +2,26 @@ import React from "react";
 import "./style.css";
 
 const whoDoes = () => <section className="whoDoes">
+    <h1 className="title">Quem Faz</h1>
+    <div className="whoDoes__body">
+        <figure className="whoDoes__body__figure">
+        </figure>
+        <div className="whoDoes__body__text">
+            <h1 className="whoDoes__body__title">
+            </h1>
+            <h2 className="whoDoes__body__subtitle">
+            </h2>
+            <p className="whoDoes__body__paragraph">
+            </p>
+            <div className="whoDoes__body__text__icons">
+            </div>
+        </div>
+        <div className="whoDoes__team">
+        </div>
+        
 
+
+    </div>
 
 </section>;
 
