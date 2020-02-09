856c285500e3c75780569bba88ab4bac83259a7e
simoneas02 <simoneas02@hotmail.com>
2020-02-09 13:10:29 -0300

    Updates

---
+++ precache-manifest.253f5c07436901db9e63327fba2a9d1f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194e85549ff5001a29ad6f3aa8d0ac36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db62588ff74b288e2c70",
+    "url": "/static/css/main.f54bdb69.chunk.css"
+  },
+  {
+    "revision": "fb60fe116f7c2f5bf058",
+    "url": "/static/js/2.75d02df3.chunk.js"
+  },
+  {
+    "revision": "db62588ff74b288e2c70",
+    "url": "/static/js/main.611b17fc.chunk.js"
+  },
+  {
+    "revision": "a7c4c37b63953c3e953f",
+    "url": "/static/js/runtime-main.0e2ae88d.js"
+  },
+  {
+    "revision": "21ba73a51ecc6200a9b4aa226cf12b37",
+    "url": "/static/media/about.21ba73a5.png"
+  },
+  {
+    "revision": "537e24dc7b0992ef48f269d4ad2967a2",
+    "url": "/static/media/facebook.537e24dc.svg"
+  },
+  {
+    "revision": "0f59034e94ffe22908f86b1d81163545",
+    "url": "/static/media/hamburger.0f59034e.svg"
+  },
+  {
+    "revision": "e5a868cb28e6452ee00b2f3332013d78",
+    "url": "/static/media/hero_max.e5a868cb.png"
+  },
+  {
+    "revision": "f1580010a7f9192f2c2dffc7299b7a36",
+    "url": "/static/media/instagram.f1580010.svg"
+  },
+  {
+    "revision": "1ac7fef1848a015ada70df7aba341889",
+    "url": "/static/media/logo.1ac7fef1.svg"
+  },
+  {
+    "revision": "8a25e27c89a8f5669e8eb82d2666559e",
+    "url": "/static/media/logo.8a25e27c.png"
+  }
+]);
