8b54ccab171f3c85c4ff56724ba00be07b74d611
Deborah Almeida <deborahsoares01@gmail.com>
2020-02-27 20:47:55 -0300

    add structure of style and index

---
+++ src/components/results/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+const results = () => (
+  <section className="container_results">
+    <h1 className="title_results">Resultados</h1>
+        <p className="text_results">O conteúdo das imagens é de propriedade exclusiva do Projeto 
+        Micropigmentação Solidária e seu uso foi autorizado pelas 
+        usuárias. As fotografias são protegidas por leis de direito 
+        patrimonial e não podem ser copiadas ou transmitidas
+         por qualquer meio sem a prévia autorização</p>
+  </section>
+)
+
+export default results
