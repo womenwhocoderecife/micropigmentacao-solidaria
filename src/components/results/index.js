090e055b4a61f3431bf15cd0ec3b95228136e747
Deborah Almeida <deborahsoares01@gmail.com>
2020-02-28 10:21:15 -0300

    add other library for carousel

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import foto1 from './assets/1.png'
@@ -27,5 +27,52 @@ class Results extends Component {
         </Carousel>
         );
     }
+}
+export default Results
+*/
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import foto1 from './assets/1.png'
+import './style.css'
+
+
+
+const slideImages = [
+    'assets/1.png',
+    'assets/2.png',
+    'assets/3.png'
+];
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true,
+  onChange: (oldIndex, newIndex) => {
+    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+  }
+}
+
+const Results = () => {
+    return (
+      <div className="slide-container">
+        <Slide {...properties}>
+          <div className="each-slide">
+            <img className="foto1" src={foto1}/>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+              <span>Slide 2</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+              <span>Slide 3</span>
+            </div>
+          </div>
+        </Slide>
+      </div>
+    )
 }
 export default Results
