30a6bb7f613579d5f2f806c832f6fa7b97f237e1
simoneas02 <simoneas02@hotmail.com>
2020-03-08 20:58:56 -0300

    add about image to main assets

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,29 +1,34 @@
-import React from "react";
-import "./style.css";
-import about from "../assets/img/about.png";
+import React from 'react'
+import './style.css'
+import about from '../../assets/imgs/about.png'
 
-const About = () => <section className="about">
-
+const About = () => (
+  <section className="about">
     <div className="about__text">
+      <h2 className="about__text__title">Sobre Nós</h2>
 
-        <h2 className="about__text__title">
-            Sobre Nós
-        </h2>
-
-        <p className="about__text__paragraph">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat  incididunt ut laboret.
-            <br />
-            <br />
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat  incididunt ut laboret.
-        </p>
-
+      <p className="about__text__paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat incididunt ut laboret.
+      </p>
+      <p className="about__text__paragraph">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat incididunt ut laboret.
+      </p>
     </div>
 
     <figure className="about__image">
-
-            <img className="about__image__src" src={about} alt="Três mulheres sorrindo" />
-
+      <img
+        className="about__image__src"
+        src={about}
+        alt="Três mulheres sorrindo"
+      />
     </figure>
-</section>;
+  </section>
+)
 
-export default About;
+export default About
