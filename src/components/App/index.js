3a65623baf07f4f01874a313998b5198b3709e61
simoneas02 <simoneas02@hotmail.com>
2020-05-15 09:35:24 -0300

    update dependecies and fix the WhoDoes component path

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,14 @@
-import React from "react";
-import Header from "../Header";
-import Hero from "../Hero";
-import About from "../About";
-import WhoDoes from '../whoDoes'
+import React from 'react'
+import Header from '../Header'
+import Hero from '../Hero'
+import About from '../About'
+import WhoDoes from '../WhoDoes'
 import ImpactedWomen from '../ImpactedWomen'
-import Results from "../Result";
-import HowToParticipate from "../HowToParticipate"
-import HowToHelp from "../HowToHelp";
-import Faq from "../Faq";
-import Footer from "../Footer";
-
+import Results from '../Result'
+import HowToParticipate from '../HowToParticipate'
+import HowToHelp from '../HowToHelp'
+import Faq from '../Faq'
+import Footer from '../Footer'
 
 const App = () => (
   <>
@@ -18,12 +17,12 @@ const App = () => (
     <About />
     <WhoDoes />
     <ImpactedWomen />
-    <Results/>
-    <HowToParticipate/>
-    <HowToHelp/>
+    <Results />
+    <HowToParticipate />
+    <HowToHelp />
     <Faq />
     <Footer />
   </>
-);
+)
 
 export default App
