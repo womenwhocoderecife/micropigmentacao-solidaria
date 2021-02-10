047d00b9528d16061d798ca0b26e0ab41049ff7a
Willany Silva <willany_lp@hotmail.com>
2021-02-09 21:46:22 -0300

    Fix link

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import './style.css'
-import icone from '../../assets/imgs/icone_play.png'
+import React from "react";
+import "./style.css";
+import icone from "../../assets/imgs/icone_play.png";
 
 const Hero = () => (
   <section className="container_hero">
-    <section className = "content_hero">
+    <section className="content_hero">
       <h1 className="title_hero">Bonita é ser valente</h1>
       <p className="text_hero">
-        Mulher bonita é a que bem se quer!
-        ame-se, toque-se, a prevenção é o melhor caminho.
+        Mulher bonita é a que bem se quer! ame-se, toque-se, a prevenção é o
+        melhor caminho.
       </p>
-      <div className="link_hero" href="a">
+      <div className="link_hero">
         <p className="command_hero"> Scroll Down </p>
-        <a className="anchor_hero" href="a">
-          <img className="icon_hero" src={icone} alt="Icone_rolagem_para_baixo" />
+        <a className="anchor_hero" href="#about">
+          <img
+            className="icon_hero"
+            src={icone}
+            alt="Icone_rolagem_para_baixo"
+          />
         </a>
       </div>
     </section>
-
   </section>
-)
+);
 
-export default Hero
+export default Hero;
